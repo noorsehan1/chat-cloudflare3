aa318734a7ffb2fde0b90037c78edddf0aa2de99
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2026-06-29 14:39:41 +0800

    Update game-server.js

--- game-server.js
+++ game-server.js
@@ -1,6 +1,6 @@
 // ==================== GAME SERVER - DURABLE OBJECT ====================
 
-const CONSTANTS = {
+ const CONSTANTS = {
   MAX_LOWCARD_GAMES: 10,
   REGISTRATION_TIME_MS: 20000,
   DRAW_TIME_MS: 20000,
