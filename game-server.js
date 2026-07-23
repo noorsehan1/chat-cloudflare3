d615e7cf6a3ba1bf725514e6e75fb8f395461aa5
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2026-07-23 15:53:27 +0800

    Update game-server.js

--- game-server.js
+++ game-server.js
@@ -1,6 +1,6 @@
 // ==================== GAME-SERVER.JS ====================
 
-const CONSTANTS = {
+  bconst CONSTANTS = {
   MAX_LOWCARD_GAMES: 10,
   REGISTRATION_TIME_MS: 20000,
   DRAW_TIME_MS: 20000,
